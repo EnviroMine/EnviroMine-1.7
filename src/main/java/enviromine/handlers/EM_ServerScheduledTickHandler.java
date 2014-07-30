4bac2d0250b318ce0ba63f2f60831889f40115be
thislooksfun <thislooksfun@repbot.org>
2014-07-29 22:35:39 -0500

    Fixed ticktype

--- src/main/java/enviromine/handlers/EM_ServerScheduledTickHandler.java
+++ src/main/java/enviromine/handlers/EM_ServerScheduledTickHandler.java
@@ -1,14 +1,14 @@
 package enviromine.handlers;
 
-import enviromine.core.EM_Settings;
-
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
+
+import enviromine.core.EM_Settings;
 
 public class EM_ServerScheduledTickHandler
 {
 	@SubscribeEvent
-	public void tickEnd(TickEvent tick)
+	public void tickEnd(TickEvent.WorldTickEvent tick)
 	{
 		if(tick.side.isServer() && EM_Settings.enablePhysics) {
 			EM_PhysManager.updateSchedule();

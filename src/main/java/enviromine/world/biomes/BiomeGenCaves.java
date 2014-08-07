7ea6eaf325c3aa0192dbb50b0e1b455ba5ad7867
thislooksfun <thislooksfun@repbot.org>
2014-08-07 12:20:29 -0500

    Removed unused import

--- src/main/java/enviromine/world/biomes/BiomeGenCaves.java
+++ src/main/java/enviromine/world/biomes/BiomeGenCaves.java
@@ -2,7 +2,6 @@ package enviromine.world.biomes;
 
 import net.minecraft.entity.passive.EntityBat;
 import net.minecraft.world.biome.BiomeGenBase;
-import net.minecraft.world.biome.SpawnListEntry;
 
 public class BiomeGenCaves extends BiomeGenBase
 {

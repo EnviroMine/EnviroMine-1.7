f24f1147fe088ebcc34db18c5be22633c579aa57
Funwayguy <FunwayguyEmail@gmail.com>
2014-08-12 07:34:30 +1000

    Fixed armor properties not working at all

--- src/main/java/enviromine/handlers/EM_StatusManager.java
+++ src/main/java/enviromine/handlers/EM_StatusManager.java
@@ -837,9 +837,9 @@ public class EM_StatusManager
 					}
 				}
 				
-				if(EM_Settings.armorProperties.containsKey(Item.itemRegistry.getNameForObject(helmet)))
+				if(EM_Settings.armorProperties.containsKey(Item.itemRegistry.getNameForObject(helmet.getItem())))
 				{
-					ArmorProperties props = EM_Settings.armorProperties.get(Item.itemRegistry.getNameForObject(helmet));
+					ArmorProperties props = EM_Settings.armorProperties.get(Item.itemRegistry.getNameForObject(helmet.getItem()));
 					
 					if(isDay)
 					{
@@ -893,9 +893,9 @@ public class EM_StatusManager
 					}
 				}
 				
-				if(EM_Settings.armorProperties.containsKey(Item.itemRegistry.getNameForObject(plate)))
+				if(EM_Settings.armorProperties.containsKey(Item.itemRegistry.getNameForObject(plate.getItem())))
 				{
-					ArmorProperties props = EM_Settings.armorProperties.get(Item.itemRegistry.getNameForObject(plate));
+					ArmorProperties props = EM_Settings.armorProperties.get(Item.itemRegistry.getNameForObject(plate.getItem()));
 					
 					if(isDay)
 					{
@@ -946,9 +946,9 @@ public class EM_StatusManager
 					}
 				}
 				
-				if(EM_Settings.armorProperties.containsKey(Item.itemRegistry.getNameForObject(legs)))
+				if(EM_Settings.armorProperties.containsKey(Item.itemRegistry.getNameForObject(legs.getItem())))
 				{
-					ArmorProperties props = EM_Settings.armorProperties.get(Item.itemRegistry.getNameForObject(legs));
+					ArmorProperties props = EM_Settings.armorProperties.get(Item.itemRegistry.getNameForObject(legs.getItem()));
 					
 					if(isDay)
 					{
@@ -999,9 +999,9 @@ public class EM_StatusManager
 					}
 				}
 				
-				if(EM_Settings.armorProperties.containsKey(Item.itemRegistry.getNameForObject(boots)))
+				if(EM_Settings.armorProperties.containsKey(Item.itemRegistry.getNameForObject(boots.getItem())))
 				{
-					ArmorProperties props = EM_Settings.armorProperties.get(Item.itemRegistry.getNameForObject(boots));
+					ArmorProperties props = EM_Settings.armorProperties.get(Item.itemRegistry.getNameForObject(boots.getItem()));
 					
 					if(isDay)
 					{

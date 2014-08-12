91c59987f130ca349bc41cf2ff2eca5edce065d3
GenDeathorw <kennywd.lmt@gmail.com>
2014-08-11 19:31:01 -0500

    *Checking for Player OPPed or not or client side For Main menu settings *Rewording of en_US.lang

--- src/main/java/enviromine/gui/menu/EM_Gui_Menu.java
+++ src/main/java/enviromine/gui/menu/EM_Gui_Menu.java
@@ -1,7 +1,10 @@
 package enviromine.gui.menu;
 
+import enviromine.core.EnviroMine;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.StatCollector;
 
 public class EM_Gui_Menu extends GuiScreen
@@ -15,10 +18,25 @@ public class EM_Gui_Menu extends GuiScreen
 	@Override
 	public void initGui()
 	{
+		GuiButton serverSettings = new GuiButton(100, this.width / 2 - 75, this.height / 6 + 74 - 6, 150, 20, StatCollector.translateToLocal("emoptions.configSettings"));
+		GuiButton customEditor =  new GuiButton(100, this.width / 2 - 75, this.height / 6 + 98 - 6, 150, 20, StatCollector.translateToLocal("emoptions.customEditor"));
+		
+		if(!EnviroMine.proxy.isClient() && MinecraftServer.getServer().getConfigurationManager().func_152607_e(mc.thePlayer.getGameProfile()) || EnviroMine.proxy.isClient() )
+		{
+			serverSettings.enabled = true;
+			customEditor.enabled = true;
+		}
+		else
+		{
+			serverSettings.enabled = false;
+			customEditor.enabled = false;			
+		}
+		
 		this.buttonList.add(new GuiButton(101, this.width / 2 - 75, this.height / 6 + 50 - 6, 150, 20, StatCollector.translateToLocal("emoptions.guiOptions")));
-		this.buttonList.add(new GuiButton(100, this.width / 2 - 75, this.height / 6 + 74 - 6, 150, 20, StatCollector.translateToLocal("emoptions.configSettings")));
-		this.buttonList.add(new GuiButton(100, this.width / 2 - 75, this.height / 6 + 98 - 6, 150, 20, StatCollector.translateToLocal("emoptions.customEditor")));
+		this.buttonList.add(serverSettings);
+		this.buttonList.add(customEditor);
 		this.buttonList.add(new GuiButton(200, this.width / 2 - 100, this.height / 6 + 168, StatCollector.translateToLocal("gui.done")));
+		
 		
 	}
 	

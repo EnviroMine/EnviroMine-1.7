9ff230604a6f61d4d324efacea26005cf238e6a2
GenDeathorw <kennywd.lmt@gmail.com>
2014-08-27 23:14:23 -0500

    New Save Controler for UI Settigns

---
+++ src/main/java/enviromine/client/gui/SaveController.java
@@ -0,0 +1,107 @@
+package enviromine.client;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.crash.CrashReport;
+import net.minecraft.nbt.CompressedStreamTools;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ReportedException;
+import enviromine.client.api.HUDRegistry;
+import enviromine.client.api.HudItem;
+import enviromine.core.EnviroMine;
+
+public class SaveController {
+    protected static final String dirName = Minecraft.getMinecraft().mcDataDir + File.separator + "config" + File.separator + "AdvancedHud";
+    protected static File dir = new File(dirName);
+
+    public static boolean loadConfig(String name) {
+        return loadConfig(name, null);
+    }
+
+    public static boolean loadConfig(String name, String dirName) {
+        if (dirName != null) {
+            HUDRegistry.getMinecraftInstance();
+            dir = new File(Minecraft.getMinecraft().mcDataDir + File.separator + dirName);
+        }
+
+        String fileName = name + ".dat";
+        File file = new File(dir, fileName);
+
+        if (!file.exists()) {
+        	EnviroMine.logger.warn("Config load canceled, file does not exist. This is normal for first run.");
+            return false;
+        } else {
+        	EnviroMine.logger.info("Config load successful.");
+        }
+        try {
+            NBTTagCompound nbt = CompressedStreamTools.readCompressed(new FileInputStream(file));
+
+            HUDRegistry.readFromNBT(nbt.getCompoundTag("global"));
+
+            for (HudItem item : HUDRegistry.getHudItemList()) {
+                NBTTagCompound itemNBT = nbt.getCompoundTag(item.getName());
+                item.loadFromNBT(itemNBT);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public static void saveConfig(String name) {
+        saveConfig(name, null);
+    }
+
+    public static void saveConfig(String name, String dirName) {
+    	EnviroMine.logger.info("Saving...");
+
+        if (dirName != null) {
+            HUDRegistry.getMinecraftInstance();
+            dir = new File(Minecraft.getMinecraft().mcDataDir + File.separator + dirName);
+        }
+
+        if (!dir.exists() && !dir.mkdirs())
+            throw new ReportedException(new CrashReport("Unable to create the configuration directories", new Throwable()));
+
+        String fileName = name + ".dat";
+        File file = new File(dir, fileName);
+
+        try {
+            NBTTagCompound nbt = new NBTTagCompound();
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+            NBTTagCompound globalNBT = new NBTTagCompound();
+            HUDRegistry.writeToNBT(globalNBT);
+            nbt.setTag("global", globalNBT);
+
+            for (HudItem item : HUDRegistry.getHudItemList()) {
+                NBTTagCompound itemNBT = new NBTTagCompound();
+                item.saveToNBT(itemNBT);
+                nbt.setTag(item.getName(), itemNBT);
+            }
+
+            CompressedStreamTools.writeCompressed(nbt, fileOutputStream);
+            fileOutputStream.close();
+        } catch (IOException e) {
+            throw new ReportedException(new CrashReport("An error occured while saving", new Throwable()));
+        }
+    }
+
+    public static File[] getConfigs() {
+        return dir.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String filename) {
+                return filename.endsWith(".dat");
+            }
+        });
+    }
+
+    static {
+        HUDRegistry.getMinecraftInstance();
+    }
+}
--- src/main/java/enviromine/client/gui/menu/UI_Settings.java
+++ src/main/java/enviromine/client/gui/menu/UI_Settings.java
@@ -2,16 +2,23 @@ package enviromine.client.gui.menu;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-
+import enviromine.client.gui.HudItem;
 import enviromine.core.EnviroMine;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.crash.CrashReport;
+import net.minecraft.nbt.CompressedStreamTools;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ReportedException;
 
 import org.apache.logging.log4j.Level;
 
@@ -213,5 +220,103 @@ public class UI_Settings {
 		return m;
 	}
 
-	
+	/**
+	 * NBT FILE SAVES
+	 */
+
+	protected static final String dirName = Minecraft.getMinecraft().mcDataDir + File.separator + "config" + File.separator + "EnviroMine";
+	protected static File dir = new File(dirName);
+
+	public static boolean loadConfig(String name) {
+		return loadConfig(name, null);
+	}
+
+	public static boolean loadConfig(String name, String dirName) {
+		if (dirName != null) {
+			EnviroMine.instance.getClass();
+			dir = new File(Minecraft.getMinecraft().mcDataDir + File.separator + dirName);
+		}
+
+		String fileName = name + ".dat";
+		File file = new File(dir, fileName);
+
+		if (!file.exists()) {
+			EnviroMine.logger
+					.warn("Config load canceled, file does not exist. This is normal for first run.");
+			return false;
+		} else {
+			EnviroMine.logger.info("Config load successful.");
+		}
+		try {
+			NBTTagCompound nbt = CompressedStreamTools
+					.readCompressed(new FileInputStream(file));
+
+			HUDRegistry.readFromNBT(nbt.getCompoundTag("global"));
+
+			for (HudItem item : HUDRegistry.getHudItemList()) {
+				NBTTagCompound itemNBT = nbt.getCompoundTag(item.getName());
+				item.loadFromNBT(itemNBT);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+
+	public static void saveConfig(String name) {
+		saveConfig(name, null);
+	}
+
+	public static void saveConfig(String name, String dirName) {
+		EnviroMine.logger.info("Saving...");
+
+		if (dirName != null) {
+			HUDRegistry.getMinecraftInstance();
+			dir = new File(Minecraft.getMinecraft().mcDataDir + File.separator
+					+ dirName);
+		}
+
+		if (!dir.exists() && !dir.mkdirs())
+			throw new ReportedException(new CrashReport(
+					"Unable to create the configuration directories",
+					new Throwable()));
+
+		String fileName = name + ".dat";
+		File file = new File(dir, fileName);
+
+		try {
+			NBTTagCompound nbt = new NBTTagCompound();
+			FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+			NBTTagCompound globalNBT = new NBTTagCompound();
+			HUDRegistry.writeToNBT(globalNBT);
+			nbt.setTag("global", globalNBT);
+
+			for (HudItem item : HUDRegistry.getHudItemList()) {
+				NBTTagCompound itemNBT = new NBTTagCompound();
+				item.saveToNBT(itemNBT);
+				nbt.setTag(item.getName(), itemNBT);
+			}
+
+			CompressedStreamTools.writeCompressed(nbt, fileOutputStream);
+			fileOutputStream.close();
+		} catch (IOException e) {
+			throw new ReportedException(new CrashReport(
+					"An error occured while saving", new Throwable()));
+		}
+	}
+
+	public static File[] getConfigs() {
+		return dir.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String filename) {
+				return filename.endsWith(".dat");
+			}
+		});
+	}
+
+	static {
+		HUDRegistry.getMinecraftInstance();
+	}
+
 }

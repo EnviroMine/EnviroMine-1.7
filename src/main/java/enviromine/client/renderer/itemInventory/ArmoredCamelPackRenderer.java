ccdb648df3a6e85ffec4c552de3f9270c0bdf936
GenDeathorw <kennywd.lmt@gmail.com>
2014-08-27 23:05:01 -0500

    +Added in Fill bar one Item Renderer

--- src/main/java/enviromine/client/renderer/itemInventory/ArmoredCamelPackRenderer.java
+++ src/main/java/enviromine/client/renderer/itemInventory/ArmoredCamelPackRenderer.java
@@ -67,14 +67,63 @@ public class ArmoredCamelPackRenderer implements IItemRenderer {
 			tessellator.draw();
 			GL11.glDisable(GL11.GL_BLEND);
 			
+			
+	        renderFillBar(itemStack);
 			// 	====================== Render text ======================
 			// 	GL11.glEnable(GL11.GL_TEXTURE_2D);
 			//  	String text = Integer.toString(itemStack.getTagCompound().getInteger("camelPackFill"));
 			//    fontRenderer.drawStringWithShadow(text, 1, 1, 0xFFFFFF);
 		}
+
+
         GL11.glEnable(GL11.GL_LIGHTING);
         GL11.glDisable(GL11.GL_ALPHA_TEST);
+        
+    }
+    
+    public void renderFillBar(ItemStack itemStack)
+    {
+    	int x = 13;
+    	int y = 2;
+    	int height = 10;
+    	int width = 1;
+		int i = itemStack.getTagCompound().getInteger("camelPackFill");
+		int max = 100;
+		int disp = (i <= 0 ? 0 : i > max ? 100 : (int)(i/(max/100F)));
 
+            double currentFill = itemStack.getTagCompound().getInteger("camelPackFill");
+            //int j1 = (int)Math.round(13.0D - disp * 13.0D);
+            int j1 = (int)Math.round(((double)height/100) * disp);
+            int k = (int)Math.round((255.0D/100) * disp);
+           
+            GL11.glDisable(GL11.GL_LIGHTING);
+            GL11.glDisable(GL11.GL_DEPTH_TEST);
+            GL11.glDisable(GL11.GL_TEXTURE_2D);
+            GL11.glDisable(GL11.GL_ALPHA_TEST);
+            GL11.glDisable(GL11.GL_BLEND);
+            Tessellator tessellator = Tessellator.instance;
+            int l = 255 - k << 16 | k << 8;
+            int i1 = (255 - k) / 4 << 16 | 16128;
+            this.renderQuad(tessellator, 0 + x, 1 + y, width+1, height, 0);
+            this.renderQuad(tessellator, 0 + x, 0 + y, width, height, EnviroUtils.getColorFromRGBA(42, 85, 210, k));
+            this.renderQuad(tessellator, 0 + x, 0 + y, width, height-j1, EnviroUtils.getColorFromRGBA(220, 3, 3, 255));
+            //GL11.glEnable(GL11.GL_BLEND); // Forge: Disable Bled because it screws with a lot of things down the line.
+            GL11.glEnable(GL11.GL_ALPHA_TEST);
+            GL11.glEnable(GL11.GL_TEXTURE_2D);
+            GL11.glEnable(GL11.GL_LIGHTING);
+            GL11.glEnable(GL11.GL_DEPTH_TEST);
+            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+    }
+    
+    private void renderQuad(Tessellator tessellator, int x1, int y1, int width, int height, int p_77017_6_)
+    {
+        tessellator.startDrawingQuads();
+        tessellator.setColorOpaque_I(p_77017_6_);
+        tessellator.addVertex((double)(x1 + 0), (double)(y1 + 0), 0.0D);
+        tessellator.addVertex((double)(x1 + 0), (double)(y1 + height), 0.0D);
+        tessellator.addVertex((double)(x1 + width), (double)(y1 + height), 0.0D);
+        tessellator.addVertex((double)(x1 + width), (double)(y1 + 0), 0.0D);
+        tessellator.draw();
     }
 
 }

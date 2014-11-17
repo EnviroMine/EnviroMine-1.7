9caef3674c723c608a18b725413ba9a12461c8d1
thislooksfun <thislooksfun@repbot.org>
2014-11-17 16:36:07 -0600

    Added @author tags

--- src/main/java/enviromine/client/renderer/tileentity/ventilation/TileEntityFanRenderer.java
+++ src/main/java/enviromine/client/renderer/tileentity/ventilation/TileEntityFanRenderer.java
@@ -1,7 +1,3 @@
-/**
- * @author thislooksfun
- */
-
 package enviromine.client.renderer.tileentity.ventilation;
 
 import enviromine.blocks.tiles.ventilation.TileEntityFan;
@@ -17,33 +13,38 @@ import codechicken.multipart.TileMultipart;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
 
+/**
+ * @author thislooksfun
+ */
 public class TileEntityFanRenderer extends TileEntitySpecialRenderer
 {
 	private static final ResourceLocation texture = new ResourceLocation("enviromine", "textures/models/blocks/fan.png");
 	private ModelFan model = new ModelFan();
-
+	
 	@Override
 	public void renderTileEntityAt(TileEntity tileEntity, double x, double y, double z, float scale)
 	{
 		VentDataHandler handler;
-		if (tileEntity instanceof TileEntityFan) {
+		if (tileEntity instanceof TileEntityFan)
+		{
 			handler = ((TileEntityFan)tileEntity).getHandler();
-		} else if (tileEntity instanceof TileMultipart) {
+		} else if (tileEntity instanceof TileMultipart)
+		{
 			handler = ((VentBasePart)((TileMultipart)tileEntity).jPartList().get(0)).getHandler();
 		} else
 		{
 			return;
 		}
-
+		
 		this.bindTexture(texture);
-
+		
 		GL11.glPushMatrix();
 		GL11.glEnable(GL12.GL_RESCALE_NORMAL);
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 		GL11.glTranslatef((float)x, (float)y + 1.0F, (float)z + 1.0F);
 		GL11.glScalef(1.0F, -1.0F, -1.0F);
 		GL11.glTranslatef(0.5F, 0.5F, 0.5F);
-
+		
 		
 		//Rotation start
 		int i = handler.provider().getCoords().getMetadata();
@@ -56,9 +57,9 @@ public class TileEntityFanRenderer extends TileEntitySpecialRenderer
 		//Rotation end
 		
 		GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
-
+		
 		this.model.renderAll(handler, 0.0625F);
-
+		
 		GL11.glDisable(GL12.GL_RESCALE_NORMAL);
 		GL11.glPopMatrix();
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);

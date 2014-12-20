9d650a5100a3ce006b501fdd89361cc42d62415a
GenDeathorw <kennywd.lmt@gmail.com>
2014-12-20 01:06:59 -0600

    *  Added Enums for Text types for colors, and line changes *  Code clean up * Added new functions to make it easier to read and write info

--- src/main/java/enviromine/client/gui/menu/update/PostGuiList.java
+++ src/main/java/enviromine/client/gui/menu/update/PostGuiList.java
@@ -4,6 +4,9 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Pattern;
+
+import org.lwjgl.opengl.GL11;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
@@ -43,33 +46,39 @@ public class PostGuiList extends GuiListExtended{
 	
 	public void EnviromineVersions(Minecraft mc)
 	{
+		if(EM_Settings.Version == "FWG_" + "EM" + "_VER")
+		{
+			addLine("Your current version is "+ EM_Settings.Version);
+			return;
+		}
+		
+		
+		@SuppressWarnings("unused")
 		int verStat = UpdateNotification.compareVersions(EM_Settings.Version, UpdateNotification.version);
 		
 		if(verStat == -1)
 		{
-			LineList.add(new PostGuiList.Row("Your current version is "+ EM_Settings.Version, null));
-			LineList.add(new PostGuiList.Row(" ",null));
-			LineList.add(new PostGuiList.Row(StatCollector.translateToLocalFormatted("updatemsg.enviromine.avalible",UpdateNotification.version), null));
-			LineList.add(new PostGuiList.Row(" ",null));
-			LineList.add(new PostGuiList.Row("Check changlog for whats new.", null));
-			LineList.add(new PostGuiList.Row(" ",null));
-			LineList.add(new PostGuiList.Row("Download new version on our wiki!", null));
-			LineList.add(new PostGuiList.Row(" ",null));
-			LineList.add(new PostGuiList.Row("https://github.com/Funwayguy/EnviroMine/wiki/Downloads", null));
-			LineList.add(new PostGuiList.Row(" ",null));
-			LineList.add(new PostGuiList.Row("https://enviromine.wordpress.com/", null));
-			
-			
+			addLine("Your current version is "+ EM_Settings.Version);
+			addBlankLines(1);
+			addLine(StatCollector.translateToLocalFormatted("updatemsg.enviromine.avalible",UpdateNotification.version));
+			addBlankLines(1);
+			addLine("Check changlog for whats new.");
+			addBlankLines(1);
+			addLine("Download new version on our wiki!");
+			addBlankLines(1);
+			addLine("https://github.com/Funwayguy/EnviroMine/wiki/Downloads");
+			addBlankLines(1);
+			addLine("https://enviromine.wordpress.com/");
 
 		} else if(verStat == 0)
 		{
-			LineList.add(new PostGuiList.Row("EnviroMine " + EM_Settings.Version + " is up to date", null));
+			addLine("EnviroMine " + EM_Settings.Version + " is up to date");
 		} else if(verStat == 1)
 		{
-			LineList.add(new PostGuiList.Row("EnviroMine " + EM_Settings.Version + " is a debug version", null));
+			addLine("EnviroMine " + EM_Settings.Version + " is a debug version");
 		} else if(verStat == -2)
 		{
-			LineList.add(new PostGuiList.Row("An error occured while parsing EnviroMine's version file!", null));
+			addLine("An error occured while parsing EnviroMine's version file!");
 		}		
 
 	}
@@ -87,27 +96,22 @@ public class PostGuiList extends GuiListExtended{
 		
 		for(WordPressPost post : UpdatePage.Posts)
 		{ 
-			LineList.add(new PostGuiList.Row(EnumChatFormatting.BOLD.UNDERLINE +  post.getTitle(), "title"));
-			LineList.add(new PostGuiList.Row(" ",null));
-			LineList.add(new PostGuiList.Row("Posted: "+ EnumChatFormatting.ITALIC + post.getPubDate(), "date"));
-			LineList.add(new PostGuiList.Row(" ",null));			
-			
-			//String test = .replaceAll("&nbsp;", "");
+			addLine(EnumChatFormatting.BOLD.UNDERLINE +  post.getTitle(), textType.TITLE);
+			addBlankLines(1);
+
+			addLine("Posted: "+ EnumChatFormatting.ITALIC + post.getPubDate(), textType.DATE);
+			addBlankLines(1);			
 			
 			List wordWrap = mc.fontRenderer.listFormattedStringToWidth(post.getDescription(),300);
 			
 			addWordWrap(wordWrap);
 			
-			LineList.add(new PostGuiList.Row(" ",null));
+			addBlankLines(1);	
 			
-			LineList.add(new PostGuiList.Row(EnumChatFormatting.ITALIC + "Posted by: "+ post.getCreator(), "creator"));
-			
-			LineList.add(new PostGuiList.Row(" ",null));
-			LineList.add(new PostGuiList.Row(" ",null));
-			LineList.add(new PostGuiList.Row(" ",null));
-			LineList.add(new PostGuiList.Row(" ",null));
-			LineList.add(new PostGuiList.Row(" ",null));
-			
+			addLine(EnumChatFormatting.ITALIC + "Posted by: "+ post.getCreator(), textType.CREATOR);
+			addBlankLines(2);
+			addLine("", textType.HR);
+			addBlankLines(2);
 		}
 		
 	}
@@ -115,16 +119,69 @@ public class PostGuiList extends GuiListExtended{
 	
 	private void addWordWrap(List wordWrap)
 	{
+		textType type;
 		Iterator wrapped = wordWrap.iterator();
 		while (wrapped.hasNext())
 		{
 			Object line = wrapped.next();
-		
+			type = textType.DEFAULT;
+			if (NewsPage.tabSelection == 152)	type = parseChangelog(line.toString());
 
-			LineList.add(new PostGuiList.Row(line.toString(),null));
+			LineList.add(new PostGuiList.Row(line.toString(),type));
 		}
 	}
 	
+	private void addBlankLines(int num)
+	{
+		for(int i = 0; i < num; i++)
+		{
+			addLine("");
+		}
+		
+	}
+	
+	private void addLine(String text)
+	{
+		addLine(text, textType.DEFAULT);
+	}
+	
+	private void addLine(String text, textType hr)
+	{
+		LineList.add(new PostGuiList.Row(text, hr));
+	}
+	
+	private textType parseChangelog(String line)
+	{
+		Pattern versionNum = Pattern.compile("\\[.+\\]");
+		Pattern change = Pattern.compile("(Fixed)");
+		Pattern add = Pattern.compile("Added");
+		Pattern removed = Pattern.compile("Removed");
+		Pattern header = Pattern.compile("Full EnviroMine Changelog");
+		
+		
+		System.out.println(line + change.matcher(line).matches());
+		if(versionNum.matcher(line).matches())
+		{
+			return textType.VERSION;
+		}
+		else if (change.matcher(line).matches())
+		{
+			return textType.CHANGED;
+		}
+		else if (header.matcher(line).matches())
+		{
+			return textType.HEADER;
+		}
+		else if (add.matcher(line).matches())
+		{
+			return textType.ADD;
+		}
+		else if (removed.matcher(line).matches())
+		{
+			return textType.REMOVED;
+		}
+		else return textType.DEFAULT;
+	}
 	@Override
 	public IGuiListEntry getListEntry(int p_148180_1_) {
 
@@ -166,41 +223,21 @@ public class PostGuiList extends GuiListExtended{
 			Minecraft mc = Minecraft.getMinecraft();
 			public static int LastYpos = 0;	
 			private String line;
-			private String type;
+			private textType type;
 
-		 	public Row(String line, String type)
+		 	public Row(String line, textType type)
 		 	{
 		 		
 		 		this.line = line;
 		 		this.type = type;
 		 	}
 	
-		 	
+		 	int color;
 	
 		 	@Override
 		 	public void drawEntry(int p_148279_1_, int p_148279_2_,	int p_148279_3_, int p_148279_4_, int p_148279_5_,	Tessellator p_148279_6_, int p_148279_7_, int p_148279_8_,	boolean p_148279_9_) 
 		 	{
-		 		
-		 		
-		 		
-		 		
-		 		if(type == "title")
-		 		{
-		 			mc.fontRenderer.drawString(line, 140, p_148279_3_, RenderAssist.getColorFromRGBA(246, 255, 0, 255) );
-		 		}
-		 		else if (type == "creator")
-		 		{
-		 			mc.fontRenderer.drawString(line, 140, p_148279_3_, RenderAssist.getColorFromRGBA(53, 219, 161, 255) );
-		 		}
-		 		else if (type == "date")
-		 		{
-		 			mc.fontRenderer.drawString(line, 140, p_148279_3_, RenderAssist.getColorFromRGBA(71, 134, 186, 255) );
-		 		}
-		 		else
-		 		{
-		 			mc.fontRenderer.drawString(line, 140, p_148279_3_,16777215 );
-		 		}
-		 		
+		 		mc.fontRenderer.drawString(textTypeText(type, line), 130, p_148279_3_ , textTypeColor(type));
 		 	}
 
 		 	@Override
@@ -216,7 +253,83 @@ public class PostGuiList extends GuiListExtended{
 		 		// 	TODO Auto-generated method stub
 			
 		 	}
+		 	
+		 
 	   }
 	   
+	 enum textType
+	 {
+		 TITLE,
+		 DATE,
+		 CREATOR,
+		 HR,
+		 DEFAULT,
+		 
+		 VERSION,
+		 HEADER,
+		 ADD,
+		 REMOVED,
+		 CHANGED
+		 
+		 
+	 }
+	 
+	 public static int textTypeColor(textType type)
+	 {
+		 switch (type)
+		 {
+			case TITLE:
+				return RenderAssist.getColorFromRGBA(21, 153, 21, 255);
+			 
+			case DATE:
+				return  RenderAssist.getColorFromRGBA(71, 134, 186, 255);
+				 
+			case CREATOR:
+				return RenderAssist.getColorFromRGBA(53, 219, 161, 255);
+				 
+			case HR:
+				return RenderAssist.getColorFromRGBA(71, 134, 186, 255);
+			 
+			case VERSION:
+				return RenderAssist.getColorFromRGBA(255, 251, 0, 255);
+				
+			case HEADER:
+				return RenderAssist.getColorFromRGBA(110, 129, 255, 255);
+				
+			case ADD:
+				return RenderAssist.getColorFromRGBA(0, 255, 0, 255);
+				
+			case REMOVED:
+				return RenderAssist.getColorFromRGBA(255, 0, 0, 255);
+				
+			case CHANGED:
+				return RenderAssist.getColorFromRGBA(255, 98, 0, 255);
+				
+			 default:
+				return  16777215;
+				
+		 }
+	 }
+	 
+	 public static String textTypeText(textType type, String line)
+	 {
+		 switch (type)
+		 {
+			case TITLE:
+				line = line.toUpperCase();
+				break;
+
+			case HR:
+	 			String hr = "---------------------------------------------------------------------------------------";
+	 			line = Minecraft.getMinecraft().fontRenderer.trimStringToWidth(hr, 300);
+	 			break;
+
+			default:
+				break;
+				
+		 }
+		 
+		 return line;
+	 }
 }
 	

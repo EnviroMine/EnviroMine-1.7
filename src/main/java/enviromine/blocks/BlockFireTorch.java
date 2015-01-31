3fd72732e5ac6997d4932f04bdc6bdb234e64e46
Funwayguy <FunwayguyEmail@gmail.com>
2015-01-31 21:09:49 +1000

    Minor patch to nerf torch burnout chance

--- src/main/java/enviromine/blocks/BlockFireTorch.java
+++ src/main/java/enviromine/blocks/BlockFireTorch.java
@@ -70,7 +70,7 @@ public class BlockFireTorch extends BlockTorch
 			return;
 		}
 		
-		if((world.rand.nextInt(1000) == 0 || (world.isRaining() && world.canBlockSeeTheSky(x, y, z))) && EM_Settings.torchesGoOut)
+		if((world.rand.nextInt(10000) == 0 || (world.isRaining() && world.canBlockSeeTheSky(x, y, z))) && EM_Settings.torchesGoOut)
 		{
 			world.playSoundEffect((double)x + 0.5D, (double)y + 0.5D, (double)z + 0.5D, "random.fizz", 0.5F, 2.6F + (world.rand.nextFloat() - world.rand.nextFloat()) * 0.8F);
 			world.setBlock(x, y, z, ObjectHandler.offTorch, world.getBlockMetadata(x, y, z), 3);
